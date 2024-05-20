8fad801cd8f89a6fe38ef2aa72a224131b4c5340
Ragna Ólafsdóttir <ragnaolafsdottir@Ragnas-MBP.c.is>
2024-05-20 21:59:09 +0000

    swagger

--- Program.cs
+++ Program.cs
@@ -20,11 +20,8 @@ builder.Services.AddScoped<GuestService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
